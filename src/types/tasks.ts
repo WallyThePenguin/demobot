f5c98eddae0a25fcfd4f041012043444da49a97c
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-06-23 09:30:57 -0400

    Adding a Disabled option to schema.

--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -3,6 +3,8 @@ export interface Task {
   name: string;
   /** The amount of milliseconds to wait before running this again. */
   interval: number;
+  /** Choose whether to disable the task by default or not. */
+  disabled?: boolean;
   /** The main code that will be run when this monitor is triggered. */
   execute: () => unknown;
 }
