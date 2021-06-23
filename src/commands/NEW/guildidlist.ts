5bd5215e2acc43e21e2e8c863f5f32399aed4424
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-06-23 09:29:53 -0400

    G`looks, made it an actual list.

--- src/commands/NEW/guildidlist.ts
+++ src/commands/NEW/guildidlist.ts
@@ -1,7 +1,8 @@
-import { sendMessage, cache } from "../../../deps.ts";
+import { sendMessage, cache, bigintToSnowflake } from "../../../deps.ts";
 import { bot } from "../../../cache.ts";
 import { PermissionLevels } from "../.././types/commands.ts";
-bot.commands.set("guildlist", {
+import { createCommand } from "../../utils/helpers.ts";
+createCommand({
   name: "guildlist",
   aliases: ["gl"],
   dmOnly: false,
@@ -14,8 +15,10 @@ bot.commands.set("guildlist", {
   userChannelPermissions: [],
   description: "Lists the servers that your bot is in with server id.",
   execute: function (message) {
-    const guildave = cache.guilds.map((g) => g.unavailable);
     const guildlist = cache.guilds.map((g) => g.id);
-    sendMessage(message.channelId, `IDS: ${guildlist}, Availability: ${guildave}`);
+    const guildname = cache.guilds.map((g) => g.name);
+    message.reply(
+      `${guildname[0]}, ${guildlist[0]} \n ${guildname[1]}, ${guildlist[1]}  \n  ${guildname[2]}, ${guildlist[2]}  \n  ${guildname[3]}, ${guildlist[3]}  \n  ${guildname[4]}, ${guildlist[4]}  \n  ${guildname[5]}, ${guildlist[5]}  \n  ${guildname[6]}, ${guildlist[6]}  \n  ${guildname[7]}, ${guildlist[7]}  \n  ${guildname[8]}, ${guildlist[8]}  \n  ${guildname[9]}, ${guildlist[9]} `
+    );
   },
 });
