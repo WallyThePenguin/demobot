ef63e7f5e02f03414ae2ca8a2fdabd66743050c5
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-06-23 10:06:30 -0400

    GuildList Command Update

--- src/commands/NEW/guildidlist.ts
+++ src/commands/NEW/guildidlist.ts
@@ -14,10 +14,7 @@ createCommand({
   userChannelPermissions: [],
   description: "Lists the servers that your bot is in with server id.",
   execute: function (message) {
-    const guildlist = cache.guilds.map((g) => g.id);
-    const guildname = cache.guilds.map((g) => g.name);
-    message.reply(
-      `${guildname[0]}, ${guildlist[0]} \n ${guildname[1]}, ${guildlist[1]}  \n  ${guildname[2]}, ${guildlist[2]}  \n  ${guildname[3]}, ${guildlist[3]}  \n  ${guildname[4]}, ${guildlist[4]}  \n  ${guildname[5]}, ${guildlist[5]}  \n  ${guildname[6]}, ${guildlist[6]}  \n  ${guildname[7]}, ${guildlist[7]}  \n  ${guildname[8]}, ${guildlist[8]}  \n  ${guildname[9]}, ${guildlist[9]} `
-    );
+    const GuildArray = cache.guilds.map((g) => `${g.name}, ${g.memberCount} Members, ${g.id}`);
+    message.reply(GuildArray.join("\n"));
   },
 });
