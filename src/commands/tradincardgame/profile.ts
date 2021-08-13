d533b9992210b423d2e6aafab2754edcc333d6e2
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-08-13 05:07:35 -0400

    add level to the profile command

--- src/commands/tradincardgame/profile.ts
+++ src/commands/tradincardgame/profile.ts
@@ -1,6 +1,6 @@
 import { Embed } from "../../utils/Embed.ts";
 import { createCommand } from "../../utils/helpers.ts";
-import { runQuery, gamedatacheck } from "../../database/client.ts";
+import { runQuery, gamedatacheck, checklevel } from "../../database/client.ts";
 import { GameUserSchema } from "../../database/schemas.ts";
 createCommand({
   name: `profile`,
@@ -28,15 +28,15 @@ createCommand({
       `SELECT money, health, basicattack, abilitypower, speed, luck, chance, critchance, critdmgmultiplier, defense FROM "GameUserSchema" WHERE id = $1 LIMIT 1`,
       [member.id]
     );
+    const level = await checklevel(member.id);
     //**Make an embed with the db stats and send. */
     const statembed = new Embed()
       .setAuthor(`${message.guild?.botMember?.nick || message.guild?.bot?.tag}`, message.guild?.bot?.avatarURL)
       .setColor("random")
       .setTitle(`Stats:`)
       .setDescription(
-        `Here, You can view the current stats of ${member.mention} \n**Money:** ${stats.money} \n**Health:** ${stats.health} \n**Defense:** ${stats.defense} \n**Attack Damage:** ${stats.basicattack} \n**Ability Power:** ${stats.abilitypower} \n **Speed:** ${stats.speed} \n**Luck:** ${stats.luck} \n**Chance:** ${stats.chance} \n **Critical Chance:** ${stats.critchance} \n **Critical Damage Multiplier:** ${stats.critdmgmultiplier}`
+        `Here, You can view the current stats of ${member.mention} \n**Money:** ${stats.money} \n**Health:** ${stats.health} \n**Defense:** ${stats.defense} \n**Attack Damage:** ${stats.basicattack} \n**Ability Power:** ${stats.abilitypower} \n **Speed:** ${stats.speed} \n**Luck:** ${stats.luck} \n**Chance:** ${stats.chance} \n **Critical Chance:** ${stats.critchance} \n **Critical Damage Multiplier:** ${stats.critdmgmultiplier} \n **XP amount:** ${level.xp} \n **Level: ${level.level}`
       )
-      .setFooter(`Want to learn how stats work? Use \`!stathelp\``)
       .setTimestamp();
     message.reply({ embeds: [statembed] });
   },
