1eb74b907dbd8cd7019b74cfd5add0b64ca3f6dc
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-08-31 06:47:36 -0400

    add imagescript

--- deps.ts
+++ deps.ts
@@ -4,3 +4,4 @@ export * from "https://deno.land/x/discordeno@11.2.0/mod.ts";
 export { Manager, Player } from "https://raw.githubusercontent.com/discordeno/lavadeno/master/mod.ts";
 export type { Track } from "https://raw.githubusercontent.com/discordeno/lavadeno/master/mod.ts";
 export * from "https://deno.land/x/postgres/mod.ts";
+export * from "https://deno.land/x/imagescript@1.2.9/mod.ts";
