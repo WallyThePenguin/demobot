2bdb0836369ff6f02750fcc9f085af978919a4f5
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-09-06 02:17:58 -0400

    Inventory command for now

---
+++ src/commands/tradincardgame/inventory.ts
@@ -0,0 +1,104 @@
+import { Embed } from "../../utils/Embed.ts";
+import { createCommand, createDatabasePagination, createSubcommand } from "../../utils/helpers.ts";
+import { runQuery } from "../../database/client.ts";
+//deno-lint-ignore no-unused-vars
+import { usercardinventory, globalcardlist } from "../../database/schemas.ts";
+//UNFINISHED, FINISH BEFORE RELEASING.
+createCommand({
+  name: `inventory`,
+  aliases: [`i`, `inv`],
+  description: `Look at a users inventory.`,
+  arguments: [
+    {
+      name: "search",
+      type: "subcommand",
+      required: false,
+    },
+    {
+      name: `user`,
+      type: "member",
+      required: false,
+    },
+  ] as const,
+  guildOnly: false,
+  execute: async (message, args) => {
+    const user = args.user?.id || message.authorId;
+    //number of cards per page.
+    const cardsperpage = 10;
+
+    //Sort the rows and divide them per page.
+    const getembedcount = async (): Promise<number> => {
+      const count = Number(
+        (await runQuery(`SELECT COUNT(*) FROM "usercardinventory" WHERE userid = $1`, [user]))[0].count
+      );
+      return Math.ceil(count / cardsperpage);
+    };
+    //If theres nothing in the embed just return.
+    const getNoneEmbed = (): Promise<Embed | undefined> => {
+      // Useless stuff so its a promise, You could change the function so it does not require promise
+      //deno-lint-ignore no-unused-vars
+      return new Promise((resolve, reject) => {
+        resolve(new Embed().setTitle("No Card's Listed Under This User ID."));
+      });
+    };
+    //
+    const getEmbed = async (embedPage: number, maxPage: number): Promise<Embed> => {
+      const offset = (embedPage - 1) * cardsperpage;
+      const limit = cardsperpage;
+      const data = await runQuery<usercardinventory>(
+        `SELECT * FROM "usercardinventory" WHERE userid = $3 ORDER BY id ASC OFFSET $1 LIMIT $2`,
+        [offset, limit, user]
+      );
+      const embed = new Embed().setTitle(`<@${user}> \`s inventory`).setFooter(`Page ${embedPage} / ${maxPage}`);
+      //deno-lint-ignore no-unused-vars
+      data.forEach((card) => {
+        const datamap = data
+          .map(
+            (card) =>
+              "**ID:** " +
+              card.id +
+              " **Name:** " +
+              card.name +
+              " **Rarity:** " +
+              card.rarity +
+              ` **Image:** [Link](${card.imagelink})` +
+              `**Unique Card ID:**` +
+              card.cardnumber
+          )
+          .join("\n");
+        embed.setDescription(datamap);
+      });
+      return embed;
+    };
+    await createDatabasePagination(message, getembedcount, getNoneEmbed, getEmbed);
+  },
+});
+//Search Subcommand
+createSubcommand(`inventory`, {
+  name: `search`,
+  arguments: [
+    {
+      name: `cardnumber`,
+      type: `number`,
+      required: true,
+    },
+  ] as const,
+  execute: async (message, args) => {
+    const arg = args.cardnumber;
+
+    //Run a query based on the boolean
+    const [search] = await runQuery<usercardinventory>(`SELECT * FROM "usercardinventory" WHERE cardnumber = $1`, [
+      arg,
+    ]);
+    if (!search) return message.reply(`Invalid ID`);
+    //Theory Below For Stat Searching.
+    //const [statsearch] = await runQuery<globalcardlist>(`SELECT * FROM "globalcardlist" WHERE name = $1 and level = $2`,[search.name, search.level])
+    //Send the info from the query.
+    const embed = new Embed()
+      .setTitle(`${search.name}`)
+      .addField("ID:", `${search.id}`)
+      .addField("Owner:", `<@${search.userid}>`)
+      .addField("Level:", `${search.level}`);
+    message.reply({ embeds: [embed] });
+  },
+});
