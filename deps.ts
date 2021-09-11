00e0b4312928ec3e69e9141b59508e1a138f7e9a
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-09-11 19:42:01 -0400

    Update Deno Postgres

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export * from "https://deno.land/std@0.95.0/fmt/colors.ts";
 export * from "https://deno.land/x/discordeno@11.2.0/mod.ts";
 export { Manager, Player } from "https://raw.githubusercontent.com/discordeno/lavadeno/master/mod.ts";
 export type { Track } from "https://raw.githubusercontent.com/discordeno/lavadeno/master/mod.ts";
-export * from "https://deno.land/x/postgres/mod.ts";
+export * from "https://deno.land/x/postgres@v0.12.0/mod.ts";
 export * from "https://deno.land/x/imagescript@1.2.9/mod.ts";
