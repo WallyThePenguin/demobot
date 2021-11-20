ed8ed7e74adf4f8f1e9d8120c28fa77db2fd9ee2
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2021-11-20 16:32:57 -0500

    Daily Shop fix  + finish

--- src/tasks/dailyshop.ts
+++ src/tasks/dailyshop.ts
@@ -7,14 +7,14 @@ bot.tasks.set(`dailyshop`, {
   name: `dailyshop`,
   // Runs this function once every 6 minutes. (1 minute for testing)
   interval: Milliseconds.MINUTE * 3,
-  disabled: true,
+  disabled: false,
   execute: async function () {
     // Only run when the bot is fully ready. In case guilds are still loading dont want to send wrong stats.
     if (!cache.isReady) return;
-    //const today = new Date();
+    const today = new Date();
     const poo = 1;
     // today.getHours() === 17 && today.getMinutes() <= 10
-    if (poo == 1) {
+    if (today.getHours() === 17 && today.getMinutes() <= 10) {
       await dailyshopreset().catch(console.error);
       return;
     } else {
